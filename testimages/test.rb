2019038c0235ca4aef18e7b701efafa0f9b4e2e3
Jason Carr <jason.carr@gmail.com>
2011-04-29 01:37:27 -0400

    changed so it renders right

--- testimages/test.rb
+++ testimages/test.rb
@@ -3,28 +3,26 @@
 require 'rubygems'
 require 'rmagick'
 
-width = 154
-height = 480
+width =  480
+height = 154
 
 f = File.open('veh_bkgnd_VW_daytime.dat','rb').read
 
 z = 16
 
-data = Array.new(width) {
-  Array.new(height) {
+data = Array.new(height) {
+  Array.new(width) {
   z += 1
     [f[z],f[z],f[z]]
     
   }
 }
 
-p data
-
 img = Magick::Image.new(width, height)
 
 data.each_with_index do |row, row_index|
   row.each_with_index do |item, column_index|
-    img.pixel_color(row_index, column_index, "rgb(#{item.join(', ')})")
+    img.pixel_color(column_index, height-1-row_index, "rgb(#{item.join(', ')})")
   end
 end
 
